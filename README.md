7e2003887889a063ef1081a34c2f0cfc04ea45fd
Scott Gress <scottmgress@gmail.com>
2014-09-22 21:39:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# sails-hook-email
+
+Email hook for [Sails JS](http://sailsjs.org), using [Nodemailer](https://github.com/andris9/Nodemailer/blob/0.7/README.md)
+
+### Installation
+
+`npm install sails-hook-email`
+
+### Usage
+
+`sails.hooks.email.send(template, data, options, cb)`
+
+Parameter      | Type                | Details
+-------------- | ------------------- |:---------------------------------
+template       | ((string))          | Relative path from `templateDir` (see "Configuration" below) to a folder containing email templates.
+data           | ((object))          | Data to use to replace template tokens
+options        | ((object))          | Email sending options (see [Nodemailer docs](https://github.com/andris9/Nodemailer/blob/0.7/README.md#e-mail-message-fields))
+cb             | ((function))        | Callback to be run after the email sends (or if an error occurs).
+
+### Configuration
+
+By default, configuration lives in `sails.config.email`.  The configuration key (`email`) can be changed by setting `sails.config.hooks['sails-hook-email'].configKey`.
+
+Parameter      | Type                | Details
+-------------- | ------------------- |:---------------------------------
+service        | ((string)) | A "well-known service" that Nodemailer knows how to communicate with (see [this list of services](https://github.com/andris9/Nodemailer/blob/0.7/README.md#well-known-services-for-smtp))
+auth | ((object)) | Authentication object as `{user:"...", pass:"..."}`
+templateDir | ((string)) | Path to view templates (defaults to `../../views/emailTemplates`)
+from | ((string)) | Default `from` email address
+testMode | ((boolean)) | Flag indicating whether the hook is in "test mode".  In test mode, email options and contents are written to a `.tmp/email.txt` file instead of being actually sent.  Defaults to `true`.
+alwaysSendTo | ((string)) | If set, all emails will be sent to this address regardless of the `to` option specified.  Good for testing live emails without worrying about accidentally spamming people.
+
+### Templates
+
+To define a new email template, create a new folder with the template name inside your `templateDir` directory, and add an **html.ejs** file inside the folder.  You may also add an optional `text.ejs` file; if none is provided, Nodemailer will attempt to create a text version of the email based on the html version.
+
+### Example
+
+Given the following **html.ejs** file contained in the folder **views/emailTemplates/testEmail**:
+
+```
+<p>Dear <%=recipientName%>,</p>
+<br/>
+<p><em>Thank you</em> for being a friend.</p>
+<p>Love,<br/><%=senderName%></p>
+```
+
+executing the command with default configuration:
+
+```
+sails.config.hooks.email(
+  "testEmail",
+  {
+    recipientName: "Joe",
+    senderName: "Sue"
+  },
+  {
+    to: "joe@example.com",
+    subject: "Hi there"
+  },
+  function(err) {console.log(err || "It worked!");}
+)
+```
+
+will result in the following email being sent to `joe@example.com`
+
+> Dear Joe,
+>
+> *Thank you* for being a friend.
+>
+> Love,
+>
+> Sue
+
+with an error being printed to the console if one occurred, otherwise "It worked!".
---
+++ index.js
@@ -0,0 +1,205 @@
+/**
+ * Module dependencies
+ */
+
+var nodemailer = require('nodemailer');
+var fs = require('fs');
+var ejs = require('ejs');
+var path = require('path');
+
+
+/**
+ * Email Hook
+ *
+ * Integration with relevant parts of the nodemailer API.
+ *
+ * For a full list of available email options see:
+ * https://github.com/andris9/Nodemailer#e-mail-message-fields
+ *
+ * @param  {App} sails
+ * @return {Object}
+ * @hook
+ */
+
+module.exports = function Email (sails) {
+
+  var transport;
+  var self = this;
+
+  return {
+
+    /**
+     * Default configuration
+     * @type {Object}
+     */
+    defaults: function() {
+      var obj = {};
+      self.configKey = sails.config.hooks['sails-hook-email'].configKey || 'email';
+      obj[self.configKey] = {
+        service: 'Gmail',
+        auth: {
+          user: 'myemailaddress@gmail.com',
+          pass: 'mypassword'
+        },
+        templateDir: path.join(__dirname, '../../views/emailTemplates'),
+        from: 'noreply@login.com',
+        testMode: true
+      };
+      return obj;
+    },
+
+
+    /**
+     * @param  {Function} cb
+     */
+    initialize: function (cb) {
+
+      // Optimization for later on: precompile all the templates here and
+      // build up a directory of named functions.
+      //
+      if (sails.config[self.configKey].testMode) {
+        transport = {
+          sendMail: function(options, cb) {
+
+            // First check the .tmp directory exists
+            fs.exists(path.join(sails.config.appPath, '.tmp'), function(status) {
+              if(!status) {
+                fs.mkdir(path.join(sails.config.appPath, '.tmp'), function(err) {
+                  if(err) return cb(err);
+                  fs.appendFile(path.join(sails.config.appPath, '.tmp/email.txt'), JSON.stringify(options)+"\n", cb);
+                });
+                return;
+              }
+
+              // Otherwise just write to the .tmp/email.txt file
+              fs.appendFile(path.join(sails.config.appPath, '.tmp/email.txt'), JSON.stringify(options)+"\n", cb);
+            });
+          }
+        };
+        return cb();
+      } else {
+
+        try {
+
+          // create reusable transport method (opens pool of SMTP connections)
+          transport = nodemailer.createTransport('SMTP',{
+            service: sails.config[self.configKey].service,
+            auth: {
+              user: sails.config[self.configKey].auth.user,
+              pass: sails.config[self.configKey].auth.pass
+            }
+          });
+
+          return cb();
+        }
+        catch (e) {
+          return cb(e);
+        }
+
+      }
+    },
+
+
+    /**
+     * Send an email.
+     * @param  {Sting}    template (a named template to render)
+     * @param  {Object}   data (data to pass into the template)
+     * @param  {Object}   options (email options including to, from, etc)
+     * @param  {Function} cb
+     */
+
+    send: function (template, data, options, cb) {
+
+      data = data || {};
+      var templateDir = sails.config[self.configKey].templateDir;
+      var templatePath = path.join(templateDir, template);
+
+      // Set some default options
+      var defaultOptions = {
+        generateTextFromHTML: true
+      };
+
+      sails.log.verbose('EMAILING:',options);
+
+      async.auto({
+
+        // Grab the HTML version of the email template
+        readHtmlTemplate: function(next) {
+          fs.readFile(templatePath + '/html.ejs', next);
+        },
+
+        // Grab the Text version of the email template
+        readTextTemplate: function(next) {
+          fs.readFile(templatePath + '/text.ejs', function(err, template) {
+            // Don't exit out if there is an error, we can generate plaintext
+            // from the HTML version of the template.
+            if(err) return next();
+            return next(null, template);
+          });
+        },
+
+        // Compile the templates using ejs
+        compileTemplates: ['readHtmlTemplate', 'readTextTemplate', function(next, results) {
+          var html;
+          var text;
+
+          // Compile the HTML template, error out if there is an issue compiling
+          try {
+            if(results.readHtmlTemplate) {
+              var htmlFn = ejs.compile(results.readHtmlTemplate.toString(), {
+                cache: true, filename: template + 'html'
+              });
+
+              html = htmlFn(data);
+            }
+          } catch(e) {
+            return next(e);
+          }
+
+          // Attempt to compile and render the text version of the template, if there is
+          // an error don't return the error because the HTML version can be used to generate
+          // a plain text version
+          try {
+            if(results.readTextTemplate) {
+              var textFn = ejs.compile(results.readTextTemplate, {
+                cache: true, filename: template + 'text'
+              });
+
+              html = textFn(data);
+            }
+          } catch(e) {}
+
+
+          return next(null, { html: html, text: text });
+        }],
+
+
+        // Send the email
+        sendEmail: ['compileTemplates', function(next, results) {
+
+          defaultOptions.html = results.compileTemplates.html;
+          if(results.compileTemplates.text) defaultOptions.text = results.compileTemplates.text;
+
+          // `options`, e.g.
+          // {
+          //   to: 'somebody@example.com',
+          //   from: 'other@example.com',
+          //   subject: 'Hello World'
+          // }
+          var mailOptions = _.defaults(options, defaultOptions);
+          mailOptions.to = sails.config[self.configKey].alwaysSendTo || mailOptions.to;
+
+          transport.sendMail(mailOptions, next);
+        }]
+
+      },
+
+      // ASYNC callback
+      function(err, results) {
+        if(err) return cb(err);
+        cb(null, results.sendEmail);
+      });
+    }
+
+  };
+};
